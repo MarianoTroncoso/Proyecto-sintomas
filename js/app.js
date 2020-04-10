f1331af19e7306a78c9e6ebfd0fb840218127153
Mariano <petotronco@gmail.com>
2020-04-10 20:11:11 -0300

    creacion de la base de datos

--- js/app.js
+++ js/app.js
@@ -10,3 +10,30 @@ const form = document.querySelector('form'),
     sintomas = document.querySelector('#sintomas'),
     citas = document.querySelector('#citas'),
     headingAdministra = document.querySelector('#administra');
+
+// esperar por el DOM ready 
+document.addEventListener('DOMContentLoaded', () => {
+    // crear la base de datos
+    // forma parte de la ventana global
+    // los parametros son el nombre y la version 
+    let crearDB = window.indexedDB.open('citas', 1);
+    // NO debemos poner las versiones del tipo 1.1 1.2
+    // debido a que lo redondea a 1 
+    // siempre usar numeros enteros
+
+    // si hay un error enviarlo a la consola
+    crearDB.onerror = function(){
+        console.log('Hubo un error');
+    }
+
+    // si todos esta bien muestra en consola y asigna la bd
+    crearDB.onsuccess = function(){
+        console.log('todo listo');
+
+        // asignar a la base de datos 
+        DB = crearDB.result;
+        console.log(DB);
+    }
+
+
+})
